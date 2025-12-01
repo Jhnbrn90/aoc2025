e1fd18e8e9789a74e59d2e1a4f97c1925ca36a24
John Braun <john@brn.email>
2025-12-01 22:31:39 +0100

    [Day1|1] Add main file to work out the answer for actual input data

---
+++ day1/main.py
@@ -0,0 +1,16 @@
+from puzzle_1 import VaultDial
+
+def main():
+    with open('day1/puzzle_input.txt') as f:
+        puzzle_input = [line for line in f.read().split('\n') if line != '']
+
+    vault_dial = VaultDial(50)
+    vault_dial.turn_sequence(puzzle_input)
+
+    stopped_at_zero = vault_dial.recorded_stops.count(0)
+
+    print(f"The dial stopped at 0 this many times: {stopped_at_zero}")
+
+
+if __name__ == "__main__":
+    main()

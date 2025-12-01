6314b00ecd47e7cf762b2c4acce3f6b975ad9ee2
John Braun <john@brn.email>
2025-12-01 21:16:02 +0100

    [Day1|1] Add failing test (RED)

--- day1/test_puzzle_1.py
+++ day1/test_puzzle_1.py
@@ -26,3 +26,14 @@ def test_vault_dial_turn_left():
 
     vault_dial.turn_left(5)
     assert vault_dial.current_position == 15
+
+
+def test_vault_dial_turn_right_overflow():
+    # Given that we are at the highest number of the dial
+    vault_dial = VaultDial(99)
+
+    # When we turn right by a distance of one
+    vault_dial.turn_right(1)
+
+    # The vault dial should wrap around
+    assert vault_dial.current_position == 0

c76461ea3fe5b6e453bbe5dd372ac9b6006e6c9a
John Braun <john@brn.email>
2025-12-07 20:27:08 +0100

    [Day7|2] Another solution, but answer is too low

--- day7/day7_2.py
+++ day7/day7_2.py
@@ -1,16 +1,14 @@
-def step_down_grid(grid: list[list[str]], y: int = 0, touched_splitters = None):
+def step_down_grid(grid: list[list[str]], y: int = 0, overlaps: int = 0, split_count: int = 0):
     """This time we're interested in which splitters are touched.
 
     The total amount of options is calculated by 2^n, where `n` is the amount
     of touched splitters.
     """
-    touched_splitters = [] if touched_splitters is None else touched_splitters
-
     # Check if we've reached the end
     last_row = len(grid)-1
     if y == last_row:
         # we can't split the beam anymore
-        return touched_splitters
+        return grid, overlaps, split_count
 
     # Look at the columns of the current row
     for x, character in enumerate(grid[y]):
@@ -23,10 +21,14 @@ def step_down_grid(grid: list[list[str]], y: int = 0, touched_splitters = None):
             case '^':
                 # Check if we have beam to split above (y-1)
                 if grid[y-1][x] == '|':
-                    touched_splitters.append((x, y))
+                    split_count += 1
                     # Draw new beams below, on (y+1, x-1) and (y+1, x+1)
-                    grid[y+1][x-1] = '|'
-                    grid[y+1][x+1] = '|'
+                    for x_split in (x-1, x+1):
+                        if grid[y+1][x_split] == '|':
+                            # If there is a beam already, we're double counting
+                            overlaps += 1 
+                        else:
+                            grid[y+1][x_split] = '|'
             case '|':
                 # Check what is below the beam
                 if grid[y+1][x] == '.':
@@ -38,4 +40,4 @@ def step_down_grid(grid: list[list[str]], y: int = 0, touched_splitters = None):
             case _:
                 raise ValueError(f"Other character encountered: {character}.")
 
-    return step_down_grid(grid, y+1, touched_splitters)
+    return step_down_grid(grid, y+1, overlaps, split_count)
--- day7/main.py
+++ day7/main.py
@@ -4,15 +4,18 @@ from day7_2 import step_down_grid
 
 
 def main():
-    with open('day7/sample_input.txt') as f:
+    with open('day7/puzzle_input.txt') as f:
         puzzle_input = f.read()
     
     # Day 2
     grid = [list(c) for c in puzzle_input.split('\n') if c != '']
-    touched_splitters = set(step_down_grid(grid))
+    grid, overlaps, split_count = step_down_grid(grid)
 
-    amount_of_timelines = pow(2, len(touched_splitters))
-    print(f"Amount of possible timelines: {amount_of_timelines}")
+    routes = grid[-1].count('|')
+
+    timelines = routes+overlaps+split_count
+
+    print(f"Timelines: {timelines}")
 
 
 if __name__ == "__main__":

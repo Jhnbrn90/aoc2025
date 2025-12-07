e539043484c97b4ceaf8c9d6591fd435876b1ffa
John Braun <john@brn.email>
2025-12-07 01:03:22 +0100

    [Day6|2] Try to solve part 2, but the answer is too high.

--- day6/day6_2.py
+++ day6/day6_2.py
@@ -1,24 +1,47 @@
-import re
+import re 
+
+from math import prod
 
 
 def calculate_right_left_problem(problem_input: list[str]) -> int:
     symbol = problem_input.pop()
-
+    
     list_of_integers = [''.join(col) for col in zip(*problem_input)]
-    combined_math_problem = f'{symbol}'.join(list_of_integers)
+    list_of_integers = [re.sub(' ', '', i) for i in list_of_integers if i.strip()]
+    list_of_integers = [int(i) for i in list_of_integers]
 
-    return eval(combined_math_problem)
+    math_operation = {
+        '+': lambda x: sum(x),
+        '*': lambda x: prod(x),
+    }
+
+    return math_operation[symbol](list_of_integers)
 
 
 def parse_input_string_to_grid(math_problems_input: str) -> list[list[str]]:
     """Convert single string input to matrix of math problem strings."""
+    rows = math_problems_input.split('\n')
+
     matrix = []
 
-    for row in math_problems_input.split('\n'):
-        math_problem_row = [entry for entry in list(row)[::-1] if entry != ' ']
+    for row in rows:
+        row_tokens = []
+        tokens = row.split(' ')
+        padding = max([len(token) for token in tokens])
 
-        if math_problem_row != []:
-            matrix.append(math_problem_row)
+        for i, token in enumerate(tokens):
+            if len(token) < padding:
+                number_of_tokens_short = padding - len(token)
+                if i > 0 and tokens[i-1] == '' and token != '':  # padd left
+                    token_string = number_of_tokens_short * ' ' + token
+                    row_tokens.append(token_string)
+                elif i < len(tokens)-1 and tokens[i+1] == '' and token != '':  # padd left
+                    token_string = token + number_of_tokens_short * ' '
+                    row_tokens.append(token_string)
+            else:
+                row_tokens.append(token)
 
+        if not row_tokens == ['']:
+            matrix.append([token for token in row_tokens])
+        
     return matrix
-
--- day6/main.py
+++ day6/main.py
@@ -15,12 +15,12 @@ def main():
     with open('day6/sample_input.txt') as f:
         puzzle_input = f.read()
 
-    problem_grid = parse_input_string_to_grid(puzzle_input)
+    problem_grid = parse_grid_2(puzzle_input)
     math_problem_lines = transpose_math_problem_matrix(problem_grid)
 
     total = 0
     for math_problem_line in math_problem_lines:
-        total += calculate_single_line(math_problem_line)
+        total += calculate_right_left_problem(math_problem_line)
 
     print(f"Total of math problems: {total}.")
 

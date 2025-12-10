1fdd4d4cf9563d26c406d27bc89bef4e02db5974
John Braun <john@brn.email>
2025-12-10 17:07:00 +0100

    [Day10|1] Implement logic to find the button presses necessary to reach desired state

--- day_10/day10_1.py
+++ day_10/day10_1.py
@@ -1,4 +1,4 @@
-from functools import cached_property
+from functools import cache, cached_property
 
 
 def parse_input_str_to_parts(input_str: str) -> tuple[str, list[str]]:
@@ -7,12 +7,36 @@ def parse_input_str_to_parts(input_str: str) -> tuple[str, list[str]]:
     machine_part = str_parts.pop(0)  # first element ->  machine
     joltage_part = str_parts.pop()  # last element -> joltage requirements
 
+    # `str_parts` have only the buttons as list[str] remaining
     return machine_part, str_parts
+
+
+@cache
+def apply_xor_to(current_state: str, operation: str):
+    result = int(current_state, 2) ^ int(operation, 2)
+    return bin(result)[2:]  # skip '0b'
+
+
+@cache
+def button_to_binary(n_buttons: int, button_str: str) -> str:
+    """Return binary representation of button."""
+    representation = ['0'] * n_buttons
+
+    # Convert (0,3) in a list of [0, 3]
+    wires = list(map(int, button_str[1:-1].split(',')))
+
+    for i in wires:
+        representation[i] = '1'
+
+    return ''.join(representation)
+
 
 
 class Machine:
     def __init__(self, desired_state: str):
         self.desired_state_str = desired_state
+        # All machines start having all lights off
+        self.current_state = '0' * len(self.desired_state)
 
     @cached_property
     def desired_state(self):
@@ -22,8 +46,19 @@ class Machine:
         desired_state = desired_state.replace('.', '0')
         return desired_state
 
+    def reset(self):
+        self.current_state = '0' * len(self.desired_state)
 
-def apply_xor_to(current_state: str, operation: str):
-    result = int(current_state, 2) ^ int(operation, 2)
-    return bin(result)[2:]  # skip '0b'
+    def push_button(self, button: str):
+        button_operation = button_to_binary(
+            n_buttons=len(self.current_state),
+            button_str=button,
+        )
 
+        # Perform XOR binary operation on current state
+        new_state = apply_xor_to(self.current_state, button_operation)
+
+        # Calculate padding if necessary
+        padding = '0' * (len(self.current_state) - len(new_state))
+
+        self.current_state = padding + new_state
--- day_10/test_day10_1.py
+++ day_10/test_day10_1.py
@@ -1,7 +1,9 @@
 import pytest
 
+from itertools import permutations
 
 from day10_1 import (
+    button_to_binary,
     parse_input_str_to_parts,
     Machine,
     apply_xor_to,
@@ -17,7 +19,7 @@ def test_parse_input_str_to_parts(input_str: str, machine: str, buttons: list[st
     assert result == (machine, buttons)
 
 
-def test_machine_instance_internal_state():
+def test_machine_instance_desired_state():
     # Given we have a certain machine input string
     machine_input_str = '[.##.]'
 
@@ -29,6 +31,36 @@ def test_machine_instance_internal_state():
     assert machine.desired_state == '0110'
 
 
+def test_machine_instance_current_state():
+    machine = Machine('[.##.]')
+
+    # The initial state of all machines is "everything off"
+    assert machine.current_state == '0000'
+
+
+def test_button_to_binary_representation():
+    # Given we have a string representation of a button
+    button_str = '(0, 2, 5)'
+    # and we have a machine that has 10 buttons
+    n_buttons = 10
+
+    # When we retrieve the binary representation
+    binary_representation = button_to_binary(n_buttons, button_str)
+
+    # The following representation is expected
+    expected = '1010010000'
+    assert binary_representation == expected
+
+
+def test_machine_alter_state_using_button():
+    machine = Machine('[.##.]')
+    assert machine.current_state == '0000'
+
+    machine.push_button('(1, 3)')
+
+    assert machine.current_state == '0101'
+
+
 def test_apply_binary_xor():
     current_state = '0110'
     # add a class for the Machine (state?)
@@ -38,3 +70,27 @@ def test_apply_binary_xor():
     
     assert result == '1001'
 
+
+def test_find_shortest_path_to_desired_state():
+    # Given that we have an input string
+    input_str = '[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}'
+
+    # When we prepare the machine
+    machine_str, buttons = parse_input_str_to_parts(input_str) 
+    machine = Machine(machine_str)
+
+    # For all the possible permutations of pushing the buttons
+    buttons_reaching_desired_state = []
+
+    for permutation in permutations(buttons):
+        buttons_pressed_permutation = []
+        for button in permutation:
+            buttons_pressed_permutation.append(button)
+            machine.push_button(button)
+            if machine.current_state == machine.desired_state:
+                buttons_reaching_desired_state.append(buttons_pressed_permutation)
+                print(f"Reached end state, using {len(buttons_pressed_permutation)} button presses.")
+                machine.reset()
+                break
+
+    assert False, buttons_reaching_desired_state
